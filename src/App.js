dbd5b2d18523dacebabeae648cbff6ddd93c26a0
chriswoo59 <wooc99@gmail.com>
2023-04-26 18:10:23 -0400

    adding a login form (not working yet)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Dashboard from "./Dashboard";
+import Preferences from "./Preferences";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from "./Login";
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />}/>
+          <Route path="/preferences" element={<Preferences />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
